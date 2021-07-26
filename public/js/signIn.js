257282bf28f94e849c03d168e443b70b1ac7cee5
nuhamozumder <nuha.mozumder@gmail.com>
2021-07-26 17:44:03 +0000

    Redirect working from index.html to writeNote.html!

---
+++ public/js/signIn.js
@@ -0,0 +1,17 @@
+console.log("signIn.js loaded");
+
+const signInButton = document.querySelector(".button")
+console.log(signInButton);
+signInButton.addEventListener("click",  () => {
+    console.log("clicked!")
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(result => {
+        const user = result.user
+
+        console.log("login success", user)
+        window.location = "writeNote.html"
+    })
+    .catch(error => {
+        console.log("login failed", error)
+    })
+})
